c4b53718b95c06c14a2a86a15051bdae0464255f
aiuc209 <dzuiryuomdbaoy@gmail.com>
2026-04-17 22:25:44 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Test Testdan Otdimi Ha Yoki Yoq
+Topic: * `test` — testdan o‘tdimi? (`"ha"` yoki `"yo'q"`)
+"""
+
+def main():
+    print("Welcome to Test Testdan Otdimi Ha Yoki Yoq!")
+    print("Muvaffaqiyatli ishga tushirildi.")
+
+if __name__ == "__main__":
+    main()
